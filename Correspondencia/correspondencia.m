d3ba585965cc49319ef1909cb8d1b424c1f266e1
NicolasFDG <nicolas_fdg@hotmail.com>
2017-06-13 17:34:08 -0300

    subplot y varios frames.
    Se pusieron las imágenes de las 2 cámaras juntas en un subplot. Y se
    amplió flotadores a 3 frames.

--- Correspondencia/correspondencia.m
+++ Correspondencia/correspondencia.m
@@ -1,17 +1,17 @@
-close all
 clear Qi
 Qi(:,:,1)=[centros(:,:,1); ones(1,size(centros,2))];
 Qi(:,:,2)=[centros(:,:,2); ones(1,size(centros,2))];
 Lx=1024;
 
 camaras=2;
-close all
-figure(1); imagesc(im1(:,:,1))
+
+figure; 
+subplot(1,2,1); imagesc(im1(:,:,1))
 daspect([1,1,1])
 hold on
 title('Camara 1')
 
-figure(2); imagesc(im1(:,:,2))
+subplot(1,2,2); imagesc(im1(:,:,2))
 daspect([1,1,1])
 hold on
 title('Camara 2')
@@ -28,17 +28,17 @@ title('Camara 2')
         dircodigo=cd;
         load(dirparametros)
         cd(dircodigo)
-        [ F ] = matrizfundamental( R, T, A, kk, jj )
+        [ F ] = matrizfundamental( R, T, A, kk, jj ) ;
 
         %r1=rand(1,3);
         candidatos=[];
         
         for ii=1:size(Qi,2)
-            Qi(:,ii,jj)'
+           
             u1 = (Qi(:,ii,jj)')*F;
             x1 = 1:(Lx+1);
             y1 = (-u1(3)-u1(1)*x1)/(u1(2));
-            figure(kk)
+            subplot(1,2,kk)
             plot(x1, y1,'Color','r')
             
             candidatos(:,:,ii) = encontrarcandidatos(Qi(:,:,kk),x1, y1);
@@ -53,7 +53,7 @@ title('Camara 2')
             x2 = 1:(Lx+1);
             y2 = (-u2(3)-u2(1)*x2)/(u2(2));
             
-            figure(jj)
+            subplot(1,2,jj)
             plot(x2, y2,'Color','b')
             
             candidatos(:,:,ii) = encontrarcandidatos(Qi(:,:,jj),x2, y2);
--- Floaters/flotadores2.m
+++ Floaters/flotadores2.m
@@ -3,35 +3,37 @@ close all
 
 direccion = 'C:\Users\Nicolás\Desktop\Labo 6\Mediciones\07-06-17\Floaters\';
 dirparametros  = 'C:\Users\Nicolás\Desktop\07-06-2017\Damero\Parámetros Montaje Completo.mat';
+dirsimulacion = cd;
 
+for jj=1:3
 centros=[];
-for camara=1:2
+    for camara=1:2
 
-fondo = dir([direccion,'Fondo blanco\*C',num2str(camara),'S0001.tif']);
-fc = double(imread([direccion,'Fondo blanco\',fondo.name]));
+    fondo = dir([direccion,'Fondo blanco\*C',num2str(camara),'S0001.tif']);
+    fc = double(imread([direccion,'Fondo blanco\',fondo.name]));
 
-flot=dir([direccion, 'Floaters\*C',num2str(camara),'S0001.tif']);
-N=length(flot)
+    flot=dir([direccion, 'Floaters\*C',num2str(camara),'S0001.tif']);
+    N=length(flot)
 
-for jj=1:1
     flot(jj).name
-im = imread([direccion, 'Floaters\', flot(jj).name]);
+    im = imread([direccion, 'Floaters\', flot(jj).name]);
 
-im1(:,:,camara) = double(im)- fc;
-figure
-imagesc(im1(:,:,camara))
-daspect([1 1 1])
-colorbar
+    im1(:,:,camara) = double(im)- fc;
+    % figure
+    % imagesc(im1(:,:,camara))
+    % daspect([1 1 1])
+    % colorbar
 
-[centersDark, radiiDark] = imfindcircles(im1(:,:,camara),[5 20],'ObjectPolarity','dark');
-hold on
-viscircles(centersDark, radiiDark,'Color','r');
+    [centersDark, radiiDark] = imfindcircles(im1(:,:,camara),[5 20],'ObjectPolarity','dark');
+    %hold on
+    %viscircles(centersDark, radiiDark,'Color','r');
 
-centros(:,:,camara)= centersDark' ;
-end
+    centros(:,:,camara)= centersDark' ;
 
-end
+    end
 
 cd ..
 cd('Correspondencia')
 correspondencia
+cd(dirsimulacion)
+end
